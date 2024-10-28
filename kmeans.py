d3b17c9454d29faf4089e5433e7b1921c15dbee1
assafyaron <128134935+assafyaron@users.noreply.github.com>
2024-10-28 17:16:04 +0200

    Update kmeans.py
    Added function documentation

--- kmeans.py
+++ kmeans.py
@@ -2,6 +2,17 @@ import math
 import pandas as pd
 import numpy as np
 
+"""
+assigns each vector to the closest centroid,
+and returns a list of lists where each list contains
+the vectors assigned to the corresponding centroid
+@param vectors: list of vectors
+@type vectors: list of lists
+@param centroids: list of centroids
+@type centroids: list of lists
+@return: cluster assignment (list of lists of vectors)
+@rtype: list of lists (size: k*d)
+"""
 def assignClusters(vectors, centroids):
     clusters = [[] for i in range(len(centroids))] # Initialize clusters as a list of lists
     
@@ -17,6 +28,14 @@ def assignClusters(vectors, centroids):
 
     return clusters
 
+"""
+updates the centroids by taking the mean of the vectors in each cluster
+@param centroids: list of centroids
+@type centroids: list of lists
+@param clusters: list of lists where each list contains the vectors assigned to the corresponding centroid
+@return: updated centroids
+@rtype: list of lists
+"""
 def updateCentroids(centroids, clusters):
     for i in range(len(centroids)):
         if clusters[i]: # Avoid division by zero
@@ -24,6 +43,14 @@ def updateCentroids(centroids, clusters):
 
     return centroids
 
+"""
+checks if the centroids have converged
+@param centroids: list of centroids
+@type centroids: list of lists
+@param clusters: list of lists where each list contains the vectors assigned to the corresponding centroid
+@type clusters: list of lists
+@return: True if the centroids have converged, False otherwise
+"""
 def updateCentroidAndConvergence(centroids, clusters):
     convergence = True
     for i in range(len(centroids)):
@@ -37,6 +64,14 @@ def updateCentroidAndConvergence(centroids, clusters):
         centroids[i] = new_centroid
     return convergence
 
+"""
+performs k-means clustering on the given vectors
+@param vectors: list of vectors
+@type vectors: list of lists
+@param k: number of clusters
+@type k: int
+@return: list of centroids
+"""
 def doKmeans(vectors, k):
     centroids = vectors[:k].copy() # Choose first k vectors as initial centroids
 
